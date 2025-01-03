258684a64acb3f501c1f95ccf170a01b9fe543fa
wwsmiff <arnav0872@gmail.com>
2025-01-03 22:15:55 +0530

    Update fish config for distrobox

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,32 +1,40 @@
 function fish_prompt
-	set prev_status $status
-	set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
-	set_color cyan
-	echo -n (whoami)
-	set_color normal
-	echo -n "@"
-	set_color fa3eb5
-	echo -n (hostname)
-	set_color normal
-	echo -n " "
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-	
-	if test -d .git
-    	set_color yellow
-    	echo -n ' ('
-    	echo -n "$git_branch"
-    	echo -n ')'
-	end
+    if test "$CONTAINER_ID" != ""
+        set_color a020f0
+        echo -n "⬢ "
+        set_color normal
+    end
+    set prev_status $status
+    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+    set_color cyan
+    echo -n (whoami)
+    set_color normal
+    echo -n "@"
+    set_color fa3eb5
+    if test "$CONTAINER_ID" != ""
+        echo -n $CONTAINER_ID
+    else
+        echo -n (hostname)
+    end
+    set_color normal
+    echo -n " "
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
 
-	if test $prev_status -ne 0
-		set_color red
-		echo -n ' ['
-		echo -n $prev_status
-		echo -n ']'
-	end
+    if test -d .git
+        set_color yellow
+        echo -n ' ('
+        echo -n "$git_branch"
+        echo -n ')'
+    end
 
-	set_color normal
-	echo -n ' λ '
+    if test $prev_status -ne 0
+        set_color red
+        echo -n ' ['
+        echo -n $prev_status
+        echo -n ']'
+    end
+
+    set_color normal
+    echo -n ' λ '
 end
-
